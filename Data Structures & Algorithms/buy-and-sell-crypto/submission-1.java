e1ee2d37b004f43198a048110b7ea89062a7c6cb
Davide <76664886+Davide-Zamboni@users.noreply.github.com>
2026-04-01 17:11:34 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // if (prices.length() < 2) {
+        //     return 0;
+        // }
+        int i = 0;
+        int j = 1;
+
+        int result = 0;
+
+        while (j < prices.length){
+            if (prices[i] < prices [j]){
+                result = Math.max(result, prices[j] - prices[i]);
+            
+            }
+            else {
+                i = j;
+            }
+            j++;
+        }
+
+        return result;
+        
+    }
+}
