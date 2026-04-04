58dee02a0676ad3e2934a6695d2cdf6422686906
Davide <76664886+Davide-Zamboni@users.noreply.github.com>
2026-04-04 16:14:32 +0200

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    int [] cache;
+    public int climbStairs(int n) {
+        cache = new int[n];
+        for (int i = 0; i < n ; i++){
+            cache[i] = -1;
+        }
+        return dfs(n, 0);
+    }
+
+    public int dfs(int n, int i){
+        if (i == n){
+            return 1;
+        }
+        if (i > n){
+            return 0;
+        }
+
+        if (cache[i] != -1){
+            return cache[i];
+        }
+        cache[i] = dfs(n, i + 1) + dfs(n, i + 2);
+        return cache[i];
+    } 
+
+
+    
+}
